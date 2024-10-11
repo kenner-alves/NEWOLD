f5fe8aca38816a21041f402a162ebb7f8b26001f
kenner-alves <alveskenner@gmail.com>
2024-10-11 23:40:01 +0000

    lista

--- ola.c
+++ ola.c
@@ -1,6 +1,25 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void main (){
-    printf("ola mundo");
+void main() {
+    int x = 0; //criando uma variável inteira
+    printf("Valor de x: %d\n",x);
+    x++; //incrementando x em 1 unidade
+    printf("Valor de x: %d\n",x);
+    float y = 12.345678; //criando uma variárel Real
+    printf("Valor de y: %f\n",y);
+    //Imprimindo o valor de y com apenas 2 casas
+    printf("Valor de y: %.2f\n",y);
+    //1 Caractere deve estar entre apóstrofo
+    char sexo = 'M'; //variável que aceita 1 caractere
+    printf("Sexo: %c\n", sexo);
+    //Criando uma variável String (Texto)
+    char nome[30] = "Joao";
+    printf("Nome: %s\n", nome);
+    //Imprimindo a primeira letra do nome
+    printf("Primeira Letra: %c\n", nome[0]);
+    printf("\nDigite o valor de x: ");
+    scanf("%d", &x);
+    printf("O valor digitado foi %d\n", x);
+    printf("Olá Mundo!");
 }
